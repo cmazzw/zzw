afca184109c41fceab79e5f41180d57fa26cf8af
cmazzw <693042654@qq.com>
2018-02-05 21:29:31 +0800

    the first version

---
+++ src/main/java/com/vic/entity/User.java
@@ -0,0 +1,53 @@
+package com.vic.entity;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author vic
+ * @desc entity for table(user)
+ * 
+ */
+
+public class User {
+
+	private String c_data_type;
+
+	private String iiiii;
+
+	private String c_data_type_new;
+
+	private String c_data_type_name;
+
+	public String getC_data_type() {
+		return c_data_type;
+	}
+
+	public void setC_data_type(String c_data_type) {
+		this.c_data_type = c_data_type;
+	}
+
+	public String getIiiii() {
+		return iiiii;
+	}
+
+	public void setIiiii(String iiiii) {
+		this.iiiii = iiiii;
+	}
+
+	public String getC_data_type_new() {
+		return c_data_type_new;
+	}
+
+	public void setC_data_type_new(String c_data_type_new) {
+		this.c_data_type_new = c_data_type_new;
+	}
+
+	public String getC_data_type_name() {
+		return c_data_type_name;
+	}
+
+	public void setC_data_type_name(String c_data_type_name) {
+		this.c_data_type_name = c_data_type_name;
+	}
+}
